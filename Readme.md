322e95a492093d435d8df155d3f41ee01726f8d9
MatthewScargill <matthewscargilluni@gmail.com>
2025-09-25 21:42:06 +0100

    add kernel function

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 A simple but fast (ideally), multithreaded (we'll see) spectrum solver for 2D Helmholtz quantum billiard problems. Implements a Boundary Element Method (BEM) to construct boundary data, assemble boundary integral operators, and scan wavenumbers to approximate resonant modes. Includes simple spectrum and billiard visualisations. 
+
+I first wrote this in Julia for my Master's thesis. It was very slow but it worked. As soon as there was never any reason to ever look at that code ever again (graduation), I was converted to the church of the Crab. Rewriting it in Rust has just been an excuse to tinker (but if you find yourself in a situation where this is actually useful to you then i pity you dearly)
+
+
--- src/bem.rs
+++ src/bem.rs
@@ -3,11 +3,44 @@ pub fn test(){
     println!("gamer")
 }
 
-// Functions for the BEM resonance scan
-pub fn kernel(){
-    
+use num_complex::Complex64;
+use scilib::math::bessel::h1_nu; // H^{(1)}_\nu(z) 
 
+// Functions for the BEM resonance scan
+
+// Kernel function
+#[inline(always)]
+pub fn kernel(x: [f64; 2], n: [f64; 2], y: [f64; 2], k: f64) -> Complex64 {
+
+    // dx = x - y
+    let dx0 = x[0] - y[0];
+    let dx1 = x[1] - y[1];
+
+    // r = norm(dx)
+    let r = (dx0.mul_add(dx0, dx1 * dx1)).sqrt();
+
+    // jump term at singular point (dx -> 0)
+    const THRSH: f64 = 1e-16; // (1e-8)^2
+    if r < THRSH {
+        return Complex64::new(-0.5, 0.0);
+    }
+
+    // dot(x - y, n)
+    let dot = dx0.mul_add(n[0], dx1 * n[1]);
+    let scale = dot / r;
+
+    // H1^{(1)}(k r)
+    let kr = k * r; 
+    let h1 = h1_nu(1.0, Complex64::new(kr, 0.0)); // H^{(1)}_1(kr)
+
+    // imaginary coefficient
+    let coeff = Complex64::new(0.0, -(k * 0.25));
+
+    // imaginary coefficient * Hankel function * scaling factor
+    coeff * h1 * scale
 }
+
+
 
 pub fn construct_matrix() {
     
