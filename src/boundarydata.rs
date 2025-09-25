8a5f5cbde84c9655e8d71de259167e950da9f955
MatthewScargill <matthewscargilluni@gmail.com>
2025-09-25 15:19:41 +0100

    added notebooks and draft structure

--- src/boundarydata.rs
+++ src/boundarydata.rs
@@ -1,2 +1,39 @@
 // this is where the code for producing boundary data will be stored 
 // as well as high quality precalculated data to be imported for speed 
+
+// should we have a class for boundary data which all the functions run off of
+// i think that would be pretty useful
+
+
+// need to have a new class for boundary data
+
+
+// square functions 
+pub fn square_points() {
+
+
+}
+
+pub fn square_normals() {
+
+    
+}
+
+pub fn square_weights() {
+
+    
+}
+// require this sequence for different shapes
+
+
+
+// construct data takes a string argument and number of evaluation points and then produces the boundary data for that
+pub fn construct_data() {
+
+
+}
+
+
+
+// here we'll have some example data to call in the main function as const for speed 
+
